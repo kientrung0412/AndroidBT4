db38617edcc4932db772ff76384b3b59660abfb2
Hanabi <nguyenkientrung0412@gmail.com>
2020-08-17 05:16:11 +0700

    fix bug

--- app/src/main/java/com/example/androidbt4/MainActivity.java
+++ app/src/main/java/com/example/androidbt4/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.androidbt4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
--- app/src/main/java/com/example/androidbt4/PlayActivity.java
+++ app/src/main/java/com/example/androidbt4/PlayActivity.java
@@ -2,13 +2,11 @@ package com.example.androidbt4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.Random;
 
